ba7d750fd8b083799deea73876a2c449d2fbe7bb
Daniel Blatt <lynx@mailbox.org>
2023-11-13 21:12:57 +0100

    ♻️  Refactor code to avoid XO linter error

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const data = JSON.parse(fs.readFileSync('./palettes.json', 'utf8'));
 
 const items = data.flatMap(element => element.colors.map(color => {
 	const image = new Jimp(1, 1, Jimp.cssColorToHex(color.hex));
-	return image.writeAsync(`./images/${color.name}.png`)
-		.then(() => ({
-			title: color.name,
-			subtitle: color.hex,
-			arg: color.hex,
-			icon: {
-				path: `./images/${color.name}.png`,
-			},
-		}));
+	image.writeAsync(`./images/${color.name}.png`);
+	return {
+		title: color.name,
+		subtitle: color.hex,
+		arg: color.hex,
+		icon: {
+			path: `./images/${color.name}.png`,
+		},
+	};
 }));
 
-Promise.all(items).then(resolvedItems => { // eslint-disable-line unicorn/prefer-top-level-await
-	const filteredItems = alfy.inputMatches(resolvedItems, 'title');
-	alfy.output(filteredItems);
-});
+const resolvedItems = await Promise.all(items);
+const filteredItems = alfy.inputMatches(resolvedItems, 'title');
+alfy.output(filteredItems);
